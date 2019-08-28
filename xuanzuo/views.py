a79033e027508cbe2d3747e1fcd17c3aa4f503a9
jjxs <957990586@qq.com>
2019-08-28 13:17:19 +0800

    view

--- xuanzuo/views.py
+++ xuanzuo/views.py
@@ -76,11 +76,11 @@ class saveSeatApiView(APIView):
         except User.DoesNotExist:
             return Response({'message': '没有取到用户信息'}, status=status.HTTP_503)
 
-        if user_obj.filter(metting=metting_obj.id):
+        if user_obj.metting.filter(metting=metting_obj.id):
             return Response({'message': '座位已经被使用'}, status=status.HTTP_503)
 
-        metting_obj
-        user_obj.metting.filter(metting_id=metting_obj.id)
+        # metting_obj.metting_user.filter(user_id=user_id)
+        # user_obj.metting.filter(metting_id=metting_obj.id)
         user_obj.metting.add(metting_obj)
         user_obj.save()
 
