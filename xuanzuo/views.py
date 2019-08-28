f96466ae9e58808f2a3ce22621cdcd123fedbbab
jjxs <957990586@qq.com>
2019-08-28 17:44:18 +0800

    view

--- xuanzuo/views.py
+++ xuanzuo/views.py
@@ -71,7 +71,7 @@ class MettingSerializer(serializers.ModelSerializer):
 class SeatListApiView(APIView):
     def get(self, request):
         # result = Metting.objects.all().values()
-        serializer = MettingSerializer(data=Metting.objects.all())
+        serializer = MettingSerializer(data=Metting.objects.all(),many=True)
         return Response(serializer.data)
 
 class SeatApiView(APIView):
