d45a8efa31a0e7984c52c240430f5d781c7b1933
jjxs <957990586@qq.com>
2019-08-28 15:44:40 +0800

    view

--- xuanzuo/views.py
+++ xuanzuo/views.py
@@ -78,15 +78,12 @@ class SeatApiView(APIView):
         try:
             user_obj = User.objects.get(pk=int(user_id))
         except User.DoesNotExist:
-            return Response({'message': '没有取到用户信息'}, status=status.HTTP_503)
-        print("11111")
+            return Response({'message': '没有取到用户信息'}, status=status.HTTP_503_SERVICE_UNAVAILABLE)
         print(user_obj.metting.all())
         UserMetting.objects.filter(user_id=int(user_id), metting_id=metting_obj.id)
         if UserMetting.objects.filter(user_id=int(user_id), metting_id=metting_obj.id):
-            return Response({'message': '座位已经被使用'}, status=status.HTTP_503)
+            return Response({'message': '座位已经被使用'}, status=status.HTTP_504_GATEWAY_TIMEOUT)
 
-        # metting_obj.metting_user.filter(user_id=user_id)
-        # user_obj.metting.filter(metting_id=metting_obj.id)
         UserMetting.objects.create(user=user_obj, metting=metting_obj)
 
         metting_obj.result = seatArr
