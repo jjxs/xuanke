d7a46f0d36009e01cb146990b6ea23bd441ea172
jjxs <957990586@qq.com>
2019-09-11 15:13:23 +0800

    view

--- xuanke/settings.py
+++ xuanke/settings.py
@@ -79,8 +79,13 @@ WSGI_APPLICATION = 'xuanke.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'xuanke',
+        'USER': 'root',
+        'PASSWORD': '123456',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        "OPTIONS": {"init_command": "SET default_storage_engine=INNODB;"}
     }
 }
 
