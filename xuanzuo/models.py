af2ddf244a84e6fa1d03684f391e428f7218dd5a
jjxs <957990586@qq.com>
2019-09-21 11:25:54 +0800

    Revert "view"
    This reverts commit e4363925

--- xuanzuo/models.py
+++ xuanzuo/models.py
@@ -24,7 +24,7 @@ class User(AbstractUser):
     )
 
     sex = models.SmallIntegerField(choices=USER_GENDER_CHOICES, default=1, verbose_name="性别")
-    avatar = models.CharField(max_length=50, default="", null=True, blank=True, verbose_name="头像")
+    avatar = models.CharField(max_length=100, default="", null=True, blank=True, verbose_name="头像")
     openid = models.CharField(max_length=64, db_index=True, verbose_name='openid')
     metting = models.ManyToManyField(Metting, through='UserMetting')
 
