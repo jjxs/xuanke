5affb75655ca15ed4a66415328ed4aad92cf3c3a
jjxs <957990586@qq.com>
2019-09-21 11:09:05 +0800

    Revert "view"
    This reverts commit e4363925

--- xuanzuo/__init__.py
+++ xuanzuo/__init__.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 import os
-
+default_app_config = 'xuanzuo.XuanzuoConfig'
 VERBOSE_APP_NAME = u"选座系统"
 
 
@@ -8,6 +8,6 @@ def get_current_app_name(_file):
     return os.path.split(os.path.dirname(_file))[-1]
 
 
-class PrimaryBlogConfig(AppConfig):
+class XuanzuoConfig(AppConfig):
     name = VERBOSE_APP_NAME
     verbose_name = VERBOSE_APP_NAME
