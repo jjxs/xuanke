80f9d5eaa1df288d3f1714f717a74b8ecc952221
jjxs <957990586@qq.com>
2019-09-23 16:36:48 +0800

    Revert "view"
    This reverts commit e4363925

--- xuanke/urls.py
+++ xuanke/urls.py
@@ -1,27 +1,14 @@
-"""untitled URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
 from xuanzuo.views import WechatLoginView, SeatApiView, SeatListApiView
-def trigger_error(request):
-    division_by_zero = 1 / 0
+
+
+# def trigger_error(request):
+#     division_by_zero = 1 / 0
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('member/login', WechatLoginView.as_view()),
     path('seat', SeatApiView.as_view()),
     path('seatList', SeatListApiView.as_view()),
-    path('sentry-debug/', trigger_error),
+    # path('sentry-debug/', trigger_error),
 ]
