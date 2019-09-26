debce30bef364fed0aa49e63e15d7eea9aa1050a
jjxs <957990586@qq.com>
2019-09-26 08:58:54 +0800

    Revert "view"
    This reverts commit e4363925

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,4 +1,23 @@
-#!/bin/bash
+##!/bin/bash
+#set -e
+#
+#trap "kill -15 -1 && echo all proc killed" TERM KILL INT
+#
+#if [ "$1" = "start" ]; then
+#	chown -R mysql:mysql /var/lib/mysql
+#	service mysql start
+#	mysql -uroot -e "CREATE DATABASE if not exists xuanke DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;"
+#	mysql -uroot -e "CREATE USER fz@127.0.0.1 IDENTIFIED BY '123456'";
+#	mysql -uroot -e "GRANT ALL PRIVILEGES ON xuanke.* TO 'fz'@'127.0.0.1';"
+##	mysql --one-database loonflownew < /opt/workflowdemo/loonflow.sql
+#	service redis-server start
+#	service nginx start
+#	service supervisor start
+#	sleep inf & wait
+#else
+#	exec "$@"
+#fi
+
 set -e
 
 trap "kill -15 -1 && echo all proc killed" TERM KILL INT
@@ -6,9 +25,9 @@ trap "kill -15 -1 && echo all proc killed" TERM KILL INT
 if [ "$1" = "start" ]; then
 	chown -R mysql:mysql /var/lib/mysql
 	service mysql start
-	mysql -uroot -e "CREATE DATABASE if not exists xuanke DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;"
-	mysql -uroot -e "CREATE USER fz@127.0.0.1 IDENTIFIED BY '123456'";
-	mysql -uroot -e "GRANT ALL PRIVILEGES ON xuanke.* TO 'fz'@'127.0.0.1';"
+	mysql -uroot -e "CREATE DATABASE if not exists loonflownew DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;"
+	mysql -uroot -e "CREATE USER loonflownew@127.0.0.1 IDENTIFIED BY '123456'";
+	mysql -uroot -e "GRANT ALL PRIVILEGES ON loonflownew.* TO 'loonflownew'@'127.0.0.1';"
 #	mysql --one-database loonflownew < /opt/workflowdemo/loonflow.sql
 	service redis-server start
 	service nginx start
